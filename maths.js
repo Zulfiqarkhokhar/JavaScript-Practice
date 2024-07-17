354590a1053c18df0f405ccf5287ecd72ec75f9d
unknown <zulfiqarali.bscssef20@iba-suk.edu.pk>
2024-07-18 00:08:27 +0500

    Modules added

---
+++ maths.js
@@ -0,0 +1,7 @@
+export let AreaOfTraingle = (base, height) => {
+  return (1 / 2) * (base * height);
+};
+
+export let AreaOfSquare = (base, height) => {
+  return base * height;
+};
---
+++ modules.js
@@ -0,0 +1,5 @@
+import { AreaOfTraingle, AreaOfSquare } from "./maths.js";
+
+console.log(AreaOfTraingle(3, 5));
+
+console.log(AreaOfSquare(4, 4));
